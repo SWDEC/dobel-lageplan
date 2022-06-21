b3fda4b5a850438e058d6971646a5c3b77648c58
swazynski <simonwazynski@swdec.de>
2022-06-21 10:43:58 +0200

    fix importing babylon

--- components/ModelViewer.vue
+++ components/ModelViewer.vue
@@ -3,66 +3,46 @@
 </template>
 
 <script setup>
-// import babylonjsPkg from "babylonjs";
-// const {
-//   Engine,
-//   Color4,
-//   SceneLoader,
-//   Scene,
-//   ArcRotateCamera,
-//   Vector3,
-//   HemisphericLight,
-//   SSAORenderingPipeline
-// } = babylonjsPkg;
-import {
-  Engine,
-  Color4,
-  SceneLoader,
-  Scene,
-  ArcRotateCamera,
-  Vector3,
-  HemisphericLight,
-  SSAORenderingPipeline
-} from "babylonjs";
+import * as BABYLON from "babylonjs";
 import "babylonjs-loaders";
 
 const canvas = ref();
 
 onMounted(() => {
   // Load the 3D engine
-  var engine = new Engine(canvas.value, true, {
+  var engine = new BABYLON.Engine(canvas.value, true, {
     preserveDrawingBuffer: true,
     stencil: true,
   });
   // CreateScene function that creates and return the scene
   var createScene = function () {
     // Create a basic BJS Scene object
-    var scene = new Scene(engine);
+    var scene = new BABYLON.Scene(engine);
     // Create an Orbit Camera
-    var camera = new ArcRotateCamera(
+    var camera = new BABYLON.ArcRotateCamera(
       "camera1",
       3,
       1,
       100,
-      new Vector3(-40, 0, 0),
+      new BABYLON.Vector3(-40, 0, 0),
       scene
     );
     // Attach the camera to the canvas
     camera.attachControl(canvas.value, false);
     camera.panningSensibility = 30;
     // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
-    var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
     // ambient occlusion
-    var ssao = new SSAORenderingPipeline('ssaopipeline', scene, 0.95, [camera]);
+    var ssao = new BABYLON.SSAORenderingPipeline('ssaopipeline', scene, 0.95, [camera]);
     // Return the created scene
     return scene;
   };
   // call the createScene function
   var scene = createScene();
   // Make background transparent
-  scene.clearColor = new Color4(0, 0, 0, 0);
+  scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
   // load gltf model
-  SceneLoader.ImportMeshAsync("root", "/assets/3d/", "Dobel_Delta.gltf");
+  BABYLON.SceneLoader.ImportMeshAsync("root", "/assets/3d/", "Dobel_Delta.gltf");
   // run the render loop
   engine.runRenderLoop(function () {
     scene.render();
